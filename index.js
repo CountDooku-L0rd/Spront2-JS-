09fdcac3a4595a9c42f1eb3191e579b4c47dcf3f
CountDookuL0rd <leon790@mail.ru>
2026-03-26 15:18:52 +0300

    Условия вынесены в константы

--- index.js
+++ index.js
@@ -19,13 +19,9 @@ function evenOrOdd(num) {
 
 const arr = [2, 4, 5, 7]
 const str = 'Hello World'
-const objectsArr = [
-    {name: 'Щётка', price: 300, count: 5},
-    {name: 'Пила', price: 450, count: 12},
-    {name: 'Отвёртка', price: 150, count: 9},
-    {name: 'Дрель', price: 1500, count: 4},
-    {name: 'Долото', price: 270, count: 14}
-]
+const objectsArr = [{name: 'Щётка', price: 300, count: 5}, {name: 'Пила', price: 450, count: 12}, {
+    name: 'Отвёртка', price: 150, count: 9
+}, {name: 'Дрель', price: 1500, count: 4}, {name: 'Долото', price: 270, count: 14}]
 const num = 2
 const results = document.querySelectorAll(".result")
 results[0].textContent = stringReverse(str)
@@ -38,8 +34,11 @@ const age = 16
 const income = 100000
 const creditScore = 850
 const isExistingCustomer = false
-
-if ((age >= 21 && creditScore >= 650 && income >= 50000) || (isExistingCustomer === true && creditScore >= 650 && age >= 18 && income >= 50000) || (income >= 100000) || (income >= 50000 && creditScore === 850)) {
+const notABankClient = age >= 21 && creditScore >= 650 && income >= 50000
+const bankClient = isExistingCustomer === true && creditScore >= 650 && age >= 18 && income >= 50000
+const highIncome = income >= 100000
+const highCreditRating = income >= 50000 && creditScore === 850
+if (notABankClient || bankClient || highIncome || highCreditRating) {
     console.log("Кредит одобрен")
 } else {
     console.log("Кредит не одобрен")
